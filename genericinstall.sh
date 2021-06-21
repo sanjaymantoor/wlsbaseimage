64be889735047b3127d285e0ad9771cb12da2cd4
Sanjay Mantoor <36834780+sanjaymantoor@users.noreply.github.com>
2021-06-21 14:15:10 +0530

    Update genericinstall.sh

--- genericinstall.sh
+++ genericinstall.sh
@@ -90,7 +90,8 @@ function createSwap()
    sudo sed -i 's/ResourceDisk.Format=n/ResourceDisk.Format=y/g' /etc/waagent.conf
    sudo sed -i 's/ResourceDisk.EnableSwap=n/ResourceDisk.EnableSwap=y/g' /etc/waagent.conf
    #sudo systemctl restart waagent.service &
-   sudo systemctl kill -s SIGKILL waagent.service &
+   sudo pid=`ps -ef|grep '/usr/sbin/waagent' | grep -v grep | awk '{print $2}'`
+   sudo kill -9 $pid &
    sleep 5s
    echo "Verifying swapfile is created"
    if [ -f '/mnt/swapfile' ]; then
