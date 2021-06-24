767df6ffa742372ab882f51fd3048d6846ac346d
Sanjay Mantoor <36834780+sanjaymantoor@users.noreply.github.com>
2021-06-24 20:16:00 +0530

    Update genericinstall.sh

--- genericinstall.sh
+++ genericinstall.sh
@@ -90,7 +90,7 @@ function createSwap()
    sudo sed -i 's/ResourceDisk.Format=n/ResourceDisk.Format=y/g' /etc/waagent.conf
    sudo sed -i 's/ResourceDisk.EnableSwap=n/ResourceDisk.EnableSwap=y/g' /etc/waagent.conf
    #sudo sed -i 's/# Logs.Console=y/Logs.Console=n/g' /etc/waagent.conf
-   sudo systemctl restart waagent.service &
+   #sudo systemctl restart waagent.service &
    #sudo ps -ef|grep '/usr/sbin/waagent' | grep -v grep | awk '{print $2}' | xargs kill -9 $1
    sleep 5s
    echo "Verifying swapfile is created"
@@ -101,10 +101,10 @@ function createSwap()
       exit 1
    fi
    
-   #sudo dd if=/dev/zero of=/mnt/swapfile bs=2M count=1024
-   #sudo mkswap /mnt/swapfile 
-   #sudo swapon /mnt/swapfile 
-   #sudo chmod 0600 /mnt/swapfile 
+   sudo dd if=/dev/zero of=/mnt/swapfile bs=2M count=1024
+   sudo mkswap /mnt/swapfile 
+   sudo swapon /mnt/swapfile 
+   sudo chmod 0600 /mnt/swapfile 
 #   echo "Make a entry in /etc/fstab for swapfile"
 #   echo "/u01/swapfile swap swap defaults 0 0" >> /etc/fstab 
 
